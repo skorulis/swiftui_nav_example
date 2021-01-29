2f1f3d8b120ae9f04b35a633d295f26e88ee368a
Alex Skorulis <skorulis@gmail.com>
2021-01-29 17:45:22 +1100

    Initial Commit

---
+++ NavTest/NavTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavTestApp.swift
+//  NavTest
+//
+//  Created by Alexander Skorulis on 29/1/21.
+//
+
+import SwiftUI
+
+@main
+struct NavTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
